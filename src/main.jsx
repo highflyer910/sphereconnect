59c96c0911012222877785cc6ebcb26ae988f207
Thea Mushambadze <thea.mushambadze@gmail.com>
2025-07-20 20:53:44 +0400

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import { BrowserRouter } from 'react-router-dom';
+import { inject } from "@vercel/analytics"
 import App from './App.jsx';
 import { ThemeContextProvider } from './contexts/ThemeContext.jsx';
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
